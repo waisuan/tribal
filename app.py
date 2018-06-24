1d313fc9ac678777054788051700e2a14615061d
Evan Sia Wai Suan <Sia.WaiSuan@factset.com>
2018-06-24 01:46:39 +0100

    Persistent storage of mail content

--- app.py
+++ app.py
@@ -2,22 +2,27 @@ from bottle import get, post, request, route, run, static_file, error, debug, re
 import mailman
 import os
 
+
 # static set-up
 @route('/favicon.ico')
 def favicon():
     return static_file('favicon.ico', root='./resource/')
 
+
 @route('/view/:path#.+#')
 def server_static(path):
     return static_file(path, root='./resource/view/')
+
 
 @route('/js/:path#.+#')
 def server_static(path):
     return static_file(path, root='./resource/js/')
 
+
 @route('/css/:path#.+#')
 def server_static(path):
     return static_file(path, root='./resource/css/')
+
 
 # route set-up
 @get('/')
@@ -25,19 +30,21 @@ def server_static(path):
 def index(path=None):
     raise static_file('index.html', root='./resource')
 
+
 @post('/')
 def sendMail():
     try:
         req = request.json
         if req is None:
             raise ValueError
-        if not mailman._mailman_send(req['name'], req['email']):
+        if not mailman._mailman_send(req['email']):
             raise RuntimeError
     except (ValueError, KeyError, RuntimeError):
         response.status = 400
         return
     response.status = 200
     return
+
 
 @post('/admin')
 def editMail():
@@ -59,16 +66,21 @@ def editMail():
     response.status = 200
     return
 
+
 # error handling
 @error(404)
 def mistake404(code):
     return static_file('404.html', root='./app')
 
+
 # start
-TEMPLATES.clear()
+# TEMPLATES.clear()
 if os.environ.get('APP_LOCATION') == 'heroku':
     run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 elif os.environ.get('APP_LOCATION') == 'heroku_local':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    run(host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=True,
+        reloader=True)
 else:
-    run(host='localhost', port=8080, debug=True)
+    run(host='localhost', port=8080, debug=True, reloader=True)
--- mailman.py
+++ mailman.py
@@ -6,15 +6,20 @@ from email import encoders
 import dropbox
 import os
 import sys
+import shelve
 
-fromaddr     = os.environ.get('FROM_ADDR', None)
+fromaddr = os.environ.get('FROM_ADDR', None)
 fromaddr_pwd = os.environ.get('FROM_ADDR_PWD', None)
-token        = os.environ.get('TOKEN', None)
-filename     = os.environ.get('FILENAME', 'foo.pdf')
-mail_config       = os.environ.get('MAIL_CONFIG', 'mail_template.config')
-LOCAL_DIR    = './scratch/'
-DROPBOX_DIR  = '/'
+token = os.environ.get('TOKEN', None)
+filename = os.environ.get('FILENAME', None)
+LOCAL_DIR = './scratch/'
+DROPBOX_DIR = '/'
 CHUNK_SIZE = 2 * 1024 * 1024
+
+
+def _post_office():
+    return shelve.open('post_office.db', writeback=True)
+
 
 def _clean(file):
     if os.path.isfile(LOCAL_DIR + file):
@@ -22,11 +27,13 @@ def _clean(file):
     else:
         print("Error: %s not found" % file)
 
+
 def _store(title, message):
-    with open(mail_config, 'w') as mc:
-        mc.write(title   + '\n')
-        mc.write(message + '\n')
-    return
+    post_office = _post_office()
+    post_office['title'] = title
+    post_office['message'] = message
+    post_office.close()
+
 
 def _upload(file):
     dbx = dropbox.Dropbox(token)
@@ -34,50 +41,48 @@ def _upload(file):
         file_size = os.path.getsize(LOCAL_DIR + file)
         with open(LOCAL_DIR + file, 'rb') as f:
             if file_size <= CHUNK_SIZE:
-                dbx.files_upload(f.read(), DROPBOX_DIR + filename, dropbox.files.WriteMode('overwrite'))
+                dbx.files_upload(f.read(), DROPBOX_DIR + filename,
+                                 dropbox.files.WriteMode('overwrite'))
             else:
-                upload_session_start_result = dbx.files_upload_session_start(f.read(CHUNK_SIZE))
-                cursor = dropbox.files.UploadSessionCursor(session_id=upload_session_start_result.session_id,
-                                                           offset=f.tell())
+                upload_session_start_result = dbx.files_upload_session_start(
+                    f.read(CHUNK_SIZE))
+                cursor = dropbox.files.UploadSessionCursor(
+                    session_id=upload_session_start_result.session_id,
+                    offset=f.tell())
                 commit = dropbox.files.CommitInfo(path=DROPBOX_DIR + filename)
                 while f.tell() < file_size:
                     if ((file_size - f.tell()) <= CHUNK_SIZE):
-                        dbx.files_upload_session_finish(f.read(CHUNK_SIZE),
-                                                        cursor,
-                                                        commit)
+                        dbx.files_upload_session_finish(
+                            f.read(CHUNK_SIZE), cursor, commit)
                     else:
-                        dbx.files_upload_session_append_v2(f.read(CHUNK_SIZE),
-                                                           cursor)
+                        dbx.files_upload_session_append_v2(
+                            f.read(CHUNK_SIZE), cursor)
                         cursor.offset = f.tell()
     except dropbox.exceptions.ApiError:
         return False
     _clean(file)
     return True
 
+
 def _download():
     dbx = dropbox.Dropbox(token)
-    # print(dbx.users_get_current_account())
-    # for entry in dbx.files_list_folder('').entries:
-    #     print(entry.name)
     try:
-        dbx.files_download_to_file(LOCAL_DIR + filename, DROPBOX_DIR + filename)
+        dbx.files_download_to_file(LOCAL_DIR + filename,
+                                   DROPBOX_DIR + filename)
     except dropbox.exceptions.ApiError:
         return False
     return True
 
-def _construct(name, toaddr):
+
+def _construct(toaddr):
+    post_office = _post_office()
+
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     msg['To'] = toaddr
-    msg['Subject'] = "Hello, " + name
+    msg['Subject'] = post_office['title']
 
-    body = """
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Quisque rutrum lacus sed libero sollicitudin, sit amet finibus arcu iaculis.
-            In nec varius risus, ut imperdiet tortor. Donec eu est id magna tristique ultricies non quis lorem.
-            Sed pharetra quam eu libero convallis rhoncus. Nullam rutrum fermentum massa, aliquet placerat mi pharetra sit amet.
-            Nulla vitae cursus arcu. In enim odio, facilisis nec vehicula et, tempor nec tortor. Praesent sed metus feugiat, gravida diam eu, interdum libero.
-           """
+    body = post_office['message']
     msg.attach(MIMEText(body, 'plain'))
 
     attachment = open(LOCAL_DIR + filename, "rb")
@@ -85,15 +90,16 @@ def _construct(name, toaddr):
     part = MIMEBase('application', 'octet-stream')
     part.set_payload((attachment).read())
     encoders.encode_base64(part)
-    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    part.add_header('Content-Disposition',
+                    "attachment; filename= %s" % filename)
 
     msg.attach(part)
     attachment.close()
     return msg
 
-def _send(name, toaddr):
-    msg = _construct(name, toaddr)
 
+def _send(toaddr):
+    msg = _construct(toaddr)
     try:
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
@@ -110,11 +116,12 @@ def _send(name, toaddr):
     return True
 
 
-def _mailman_send(name, toaddr):
-    if  _download():
-        return _send(name, toaddr)
+def _mailman_send(toaddr):
+    if _download():
+        return _send(toaddr)
     else:
         return False
+
 
 def _mailman_store(title, message, file):
     _store(title, message)
